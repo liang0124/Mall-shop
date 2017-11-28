6343a460325cdd85423d6dd64b964b571bc6c4c4
liang0124 <2609233927@qq.com>
2017-11-28 21:38:15 +0800

    api

--- client/config/api.config.js
+++ client/config/api.config.js
@@ -1,5 +1,5 @@
 const isPro = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-  baseUrl: isPro ? 'http://www.yang.itnote.cn/api/' : 'api/'
+  baseUrl: isPro ? 'http://yang.itnote.cn/api/' : 'api/'
 }
