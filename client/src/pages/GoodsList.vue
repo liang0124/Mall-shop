ef4b5164167622cfa2c6ebe673acb4afa6b61bfe
liang0124 <2609233927@qq.com>
2017-11-29 18:04:59 +0800

    goodslist

--- client/src/pages/GoodsList.vue
+++ client/src/pages/GoodsList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <nav-bread>
-        <span>热门商品哈哈000</span>
+        <span>我的热门商品</span>
       </nav-bread>
       <div class="accessory-result-page accessory-page">
         <div class="container">
